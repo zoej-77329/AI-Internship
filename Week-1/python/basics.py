686ef14551d2a29626cb390ce5ff0a3ee6a0a1f5
Zoha jamil <jzoha80@gmail.com>
2026-08-18 19:51:10 +0500

    Add Python fundamentals

---
+++ Week-1/python/basics.py
@@ -0,0 +1,24 @@
+# Variables
+name = "zoha"
+age = 21
+
+# Data types
+integer_value = 10
+decimal_value = 10.5
+text = "Python"
+is_learning = True
+
+# Output
+print(name)
+print(age)
+print(text)
+
+# Operators
+a = 10
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
